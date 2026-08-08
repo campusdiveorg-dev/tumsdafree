76b636197a01bf6b836a40a83605925d2c2affa8
Crackertum <elishaadera4@gmail.com>
2026-08-09 01:21:36 +0300

    reviewed

--- src/app/admin/content/[table]/page.tsx
+++ src/app/admin/content/[table]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'next/navigation';
 import ProtectedRoute from '@/components/admin/ProtectedRoute';
